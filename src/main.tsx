9d8299967d727ae4f7fc0d1bb70c129ce4de8cb2
robgmiles <robgmiles@gmail.com>
2025-08-10 01:17:48 +0100

    feat: Configure basename for React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/*
+      This basename prop is the key to making routing work on GitHub Pages.
+    */}
+    <BrowserRouter basename="/geo-tagger-web/">
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
